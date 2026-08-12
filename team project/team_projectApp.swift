6d5908da3bb3228fe3a64c9313fb8624daa01993
s4029000 <s4029000@student.rmit.edu.au>
2026-08-12 11:53:02 +1000

    Initial Commit

---
+++ team project/team_projectApp.swift
@@ -0,0 +1,32 @@
+//
+//  team_projectApp.swift
+//  team project
+//
+//  Created by 马马驭昊 on 2026/8/12.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct team_projectApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ team projectTests/team_projectTests.swift
@@ -0,0 +1,19 @@
+//
+//  team_projectTests.swift
+//  team projectTests
+//
+//  Created by 马马驭昊 on 2026/8/12.
+//
+
+import Testing
+@testable import team_project
+
+struct team_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
